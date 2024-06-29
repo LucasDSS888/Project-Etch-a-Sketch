9e13f5c15c3b90b161a65731fce2b4ab91f26707
LucasDSS888 <32455059+LucasDSS888@users.noreply.github.com>
2024-06-29 07:48:57 -0300

    ✨feat: adding paint function

--- javascript.js
+++ javascript.js
@@ -1,29 +1,47 @@
-const container = document.querySelector(".container");
+function setGrid(x, y){
+    const container = document.querySelector(".container");
 
 
-for(let i=0; i < 16; i++) {
+    for(let i=1; i <= x; i++) {
 
-    const colum = document.createElement("div");
-    colum.classList.add("colum");
+        const colum = document.createElement("div");
+        colum.classList.add("colum");
 
-    // div.setAttribute("style", "color: blue; background: red; padding: 10px; flex-wrap: wrap;");
+        // div.setAttribute("style", "color: blue; background: red; padding: 10px; flex-wrap: wrap;");
 
-    container.appendChild(colum);
+        container.appendChild(colum);
 
 
-   
+    
+    }
+
+    liColum = document.querySelectorAll(".colum");
+
+    liColum.forEach(element => {
+        for(let i = 1; i <= y; i++) {
+            const line = document.createElement("div");
+            line.classList.add("line");
+            element.appendChild(line);
+        }
+    });
+
+    function paint(){
+        liLine = document.querySelectorAll(".line");
+
+        liLine.forEach(element => {
+            element.addEventListener("mouseover", () => {
+                element.setAttribute("style", "background: red;");
+            });
+        });
+    }
+    
+    paint();
+
+
 }
 
-liColum = document.querySelectorAll(".colum");
-
-console.log(liColum);
+setGrid(16, 16);
 
 
 
-liColum.forEach(element => {
-    for(let i = 0; i <16; i++) {
-        const line = document.createElement("div");
-        line.classList.add("line");
-        element.appendChild(line);
-    }
-});
+
